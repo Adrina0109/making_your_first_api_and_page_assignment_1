007e6ffe5fca537fab9b53da831969f6eac8089e
Adrina bernadin <mary.adrina@kalvium.community>
2024-12-04 15:15:40 +0530

    changes made

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Boilerplate Code for Virtual Assistant API
 const express = require('express');
+const { Query } = require('mongoose');
 const app = express();
 
 /*
@@ -33,6 +34,42 @@ Example Responses:
 Add the required logic below to complete the API.
 */
 
+
+app.get("/assistant/greet",(request,response) =>{
+  let name= request.query.name;
+  let day= new Date().getDay();
+  if(day==1)
+  {
+    let obj={
+    welcomeMessage: `Hello,  ${name}! Welcome to our assistant app!`,
+    dayMessage: "Happy Monday! Start your week with energy!"
+    };
+    return response.send(obj);
+  }
+  else if(day==5){
+    let obj=
+    {
+    welcomeMessage: `Hello,  ${name}! Welcome to our assistant app!`,
+    dayMessage: "It's Friday! The weekend is near!"
+    };
+    return response.send(obj);
+  }
+  else{
+    let obj=
+    {
+    welcomeMessage: `Hello, ${name}! Welcome to our assistant app!`,
+    dayMessage: "Have a wonderful day!"
+    };
+    return response.send(obj);
+  }
+  // console.log(request);
+  // console.log(request.query.name);
+  return response.send({name: name , message: 'Hello World'});
+});
+
+app.get('/', function (request, response){
+  return response.send('<h1>Hello World</h1>');
+});
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Virtual Assistant API is running on http://localhost:${PORT}`);
